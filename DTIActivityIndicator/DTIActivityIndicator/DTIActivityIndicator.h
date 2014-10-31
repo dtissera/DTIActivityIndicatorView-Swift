65e1bab611d53777a197629875a0504d8571bbda
vfgq8703 <david.tisserand@orange.com>
2014-10-31 22:07:18 +0100

    Initial Commit

---
+++ DTIActivityIndicator/DTIActivityIndicator/DTIActivityIndicator.h
@@ -0,0 +1,19 @@
+//
+//  DTIActivityIndicator.h
+//  DTIActivityIndicator
+//
+//  Created by David Tisserand on 31/10/2014.
+//  Copyright (c) 2014 dtissera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTIActivityIndicator.
+FOUNDATION_EXPORT double DTIActivityIndicatorVersionNumber;
+
+//! Project version string for DTIActivityIndicator.
+FOUNDATION_EXPORT const unsigned char DTIActivityIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTIActivityIndicator/PublicHeader.h>
+
+
